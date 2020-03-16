344468ea861d79ec966723fde015756d280a080d
PetrVorlicek <vorlicek.petricek@gmail.com>
2020-03-16 17:46:09 +0100

    virtualenv + unauthorized comment

--- crypto_duck/quack_forum/views.py
+++ crypto_duck/quack_forum/views.py
@@ -7,23 +7,47 @@ from quack_forum.models import QuackForum
 # Create your views here.
 
 # saves comment and returns comment models from database
-@login_required
+
 def comment(request):
     
-    form = CommentForm()
-
-    if request.method == "POST":
-        form = CommentForm(request.POST)
-        
-
-        if form.is_valid():
-            form_with_user = form.save(commit=False)
-            form_with_user.user = request.user.username
-            form_with_user.save()
+    #checking if user is loged in
+    if request.user.is_authenticated:
+            
         form = CommentForm()
 
-    # Ordering comments from oldest to newest
-    comment_list = QuackForum.objects.order_by('date_time')
+        if request.method == "POST":
+            form = CommentForm(request.POST)
+            
+
+            if form.is_valid():
+                form_with_user = form.save(commit=False)
+                form_with_user.user = request.user.username
+                form_with_user.save()
+            form = CommentForm()
+
+        # Ordering comments from oldest to newest
+        comment_list = QuackForum.objects.order_by('date_time')
 
 
-    return render(request,'quack_forum/forum.html',{'form':form,'comment_list':comment_list,})
+        return render(request,'quack_forum/forum.html',{'form':form,'comment_list':comment_list,})
+    
+    # this is the part when user is not loged in
+    else:
+        
+        form = CommentForm()
+
+        if request.method == "POST":
+            form = CommentForm(request.POST)
+            
+
+            if form.is_valid():
+                form_with_user = form.save(commit=False)
+                form_with_user.user = request.POST.get('username')
+                form_with_user.save()
+            form = CommentForm()
+
+        # Ordering comments from oldest to newest
+        comment_list = QuackForum.objects.order_by('date_time')
+
+
+        return render(request,'quack_forum/forum_unauthorized.html',{'form':form,'comment_list':comment_list,})
