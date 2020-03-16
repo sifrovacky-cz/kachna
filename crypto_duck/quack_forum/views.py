7259ac2f44405b074156f3581b3d845f1da06762
PetrVorlicek <vorlicek.petricek@gmail.com>
2020-03-16 23:17:02 +0100

    hiden password, hiden registration, comments by desc

--- crypto_duck/quack_forum/views.py
+++ crypto_duck/quack_forum/views.py
@@ -26,7 +26,7 @@ def comment(request):
             form = CommentForm()
 
         # Ordering comments from oldest to newest
-        comment_list = QuackForum.objects.order_by('date_time')
+        comment_list = QuackForum.objects.order_by('-date_time')
 
 
         return render(request,'quack_forum/forum.html',{'form':form,'comment_list':comment_list,})
